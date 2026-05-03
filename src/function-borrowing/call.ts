a11d300fb85d598550ce1c9b2e4698aac35e7803
nitish <nathnitish157@gmail.com>
2026-05-03 16:19:34 +0530

    call polyfill implementation

---
+++ src/function-borrowing/call.ts
@@ -0,0 +1,59 @@
+interface Calculator {
+    value: number,
+    add: (a: number, b: number) => number
+}
+
+const calc: Calculator = {
+    value: 100,
+    add(a, b) {
+        return a + b + this.value
+    }
+}
+
+interface myCalculator {
+    value: number;
+}
+
+const myCalc: myCalculator = {
+    value: 100
+}
+
+const res = calc.add.call(myCalc, 5, 10)
+console.log(res, 'res')
+
+//Implementation of call polyfill
+
+declare global {
+    interface Function {
+        myCall<T, A extends unknown[], R> (
+            this: (...args: A) => R,
+            thisArg: T,
+            ...args: A): R
+    }
+}
+
+Function.prototype.myCall = function<T, A extends unknown[], R>(
+    this: (...args: A) => R,
+    thisArg: T,
+    ...args: A
+): R {
+    if(typeof this !== 'function') throw new TypeError('myCall must be called on a function');
+
+    const context = thisArg === null || thisArg === undefined ? globalThis : Object(thisArg);
+
+    // unique key, need temporary property to attach function.
+    const fnKey = Symbol('fn');
+
+    // Attach function temporarily onto the object
+    (context as Record<PropertyKey, unknown>)[fnKey] = this;
+
+    let result = (context as Record<PropertyKey, (...args:A) => R>)[fnKey](...args);
+
+    // cleanup
+    delete (context as Record<PropertyKey, unknown>)[fnKey];
+
+    return result;
+}
+
+const res2 = calc.add.myCall(myCalc, 20, 50)
+console.log(res2, 'res2')
